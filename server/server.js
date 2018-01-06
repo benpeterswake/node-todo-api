7c87e43533a2990c8b90571aedd213feb4391891
Benjamin Peters <benjamin.peters@gmail.com>
2018-01-05 23:39:21 -0500

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -19,10 +19,14 @@ app.post('/todos' , (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.status(400).send(e)
+    res.status(400).send(e);
   });
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {
+  app
+};
