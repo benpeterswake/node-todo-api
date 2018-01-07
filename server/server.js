f7af0e2074f8855694deedde09776588288849db
Benjamin Peters <benjamin.peters@gmail.com>
2018-01-07 14:28:13 -0500

    add patch

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 //lib imports
-var express =  require('express');
-var parser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const express =  require('express');
+const parser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const _ = require('lodash');
+
 //local imports
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -65,6 +67,31 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
+    if (!todo){
+    return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Starting app on port ${port}`);
 });
@@ -72,3 +99,17 @@ app.listen(port, () => {
 module.exports = {
   app
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
